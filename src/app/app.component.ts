e8e2ebc79f916e0b97339c443a581b85cb3f37fe
Zachary Greenfield <fztgreen@gmail.com>
2024-09-05 13:21:46 -0500

    feat: Add material and animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatTabsModule, RouterOutlet, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
