46b5dd0d33597eaeb8d4877db31cc773efd8f39d
Zachary Greenfield <fztgreen@gmail.com>
2024-09-06 23:44:51 -0500

    feat: Various improvements

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  constructor() {
+    const iconRegistry = inject(MatIconRegistry);
+    const sanitizer = inject(DomSanitizer);
 
+    iconRegistry.addSvgIcon('bmc', sanitizer.bypassSecurityTrustResourceUrl('bmc-brand-icon.svg'));
+  }
 }
