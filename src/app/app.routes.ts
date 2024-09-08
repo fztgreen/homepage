d68d5bf653715cf13d54ace369b8cc885faa0520
Zachary Greenfield <fztgreen@gmail.com>
2024-09-08 16:55:00 -0500

    feat: use angular routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: NavigationComponent}
+];
