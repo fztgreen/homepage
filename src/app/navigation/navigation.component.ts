9627ebef3901862e253e80523e61781b5b8cb0f4
Zachary Greenfield <fztgreen@gmail.com>
2024-09-13 16:31:06 -0500

    feat: Implement fragments and navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,19 @@
 import {
   AfterViewInit,
   Component,
+  DestroyRef,
+  inject,
   ViewChild,
   ViewChildren,
 } from '@angular/core';
-import { MatTab, MatTabGroup, MatTabsModule } from '@angular/material/tabs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  MatTab,
+  MatTabChangeEvent,
+  MatTabGroup,
+  MatTabsModule,
+} from '@angular/material/tabs';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import 'hammerjs';
 import { BlogComponent } from '../blog/blog.component';
 import { ContactComponent } from '../contact/contact.component';
@@ -20,19 +29,42 @@ import { PhotographyComponent } from '../photography/photography.component';
     ContactComponent,
     BlogComponent,
     PhotographyComponent,
+    RouterModule,
   ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent implements AfterViewInit {
+  destroyRef = inject(DestroyRef);
   @ViewChild(MatTabGroup) group: any;
   @ViewChildren(MatTab) tabs: any;
   tab_num = 0;
   selected = 0;
   SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
+  home: string = 'home';
+
+  constructor(private _route: ActivatedRoute, private _router: Router) {}
 
   ngAfterViewInit() {
+    this._route.fragment
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((fragment) => {
+        this.navigateToTabFromFragment(fragment);
+      });
+
     this.tab_num = this.tabs.length;
+  }
+
+  private navigateToTabFromFragment(fragment: string | null) {
+    let tab = this.tabs.find((x: any): boolean => {
+      return x.textLabel.toLowerCase() == fragment?.toLowerCase();
+    });
+
+    for (let x = 0; x < this.tabs.length; x++) {
+      if (this.tabs.get(x) == tab) {
+        this.selected = x;
+      }
+    }
   }
 
   swipe(eType: any) {
@@ -44,5 +76,10 @@ export class NavigationComponent implements AfterViewInit {
     ) {
       this.selected++;
     }
+  }
+
+  tabChange(tab: MatTabChangeEvent) {
+    this.selected = tab.index;
+    this._router.navigate([], { fragment: tab.tab.textLabel });
   }
 }
