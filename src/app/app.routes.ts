0f49f5aadd29cd5454b6e3b0d0520063b6890c0a
Zachary Greenfield <fztgreen@gmail.com>
2024-09-13 15:04:25 -0500

    feat: Add intial game component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { GameComponent } from './game/game.component';
 
 export const routes: Routes = [
-    {path: "", component: NavigationComponent}
+    {path: "", component: NavigationComponent},
+    {path: "game", component: GameComponent}
 ];
