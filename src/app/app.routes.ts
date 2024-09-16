b9d1e748a7607f6671e4359265e712930ecfaf11
Zachary Greenfield <fztgreen@gmail.com>
2024-09-15 20:28:03 -0500

    feat: Add a not found page, redirect to home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 import { GameComponent } from './game/game.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    {path: "", component: NavigationComponent},
-    {path: "game", component: GameComponent}
+    { path: '',   redirectTo: '/home', pathMatch: 'full' },
+    {path: "home", component: NavigationComponent},
+    {path: "game", component: GameComponent},
+    {path: "**", component: NotFoundComponent}
 ];
