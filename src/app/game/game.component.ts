092a49819242aa4ab3db5bb23845ad7843d15537
Zachary Greenfield <fztgreen@gmail.com>
2024-09-15 21:49:19 -0500

    feat: Add counting game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
-export class GameComponent {}
+export class GameComponent {
+  count: number = 0;
+
+  increment(): void {
+    this.count += 1;
+  }
+}
