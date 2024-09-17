ecd3813bf48cf621f49f8ecac04c843bddce30b5
Zachary Greenfield <fztgreen@gmail.com>
2024-09-16 22:18:33 -0500

    feat: Add additional autoclicker to the game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-game',
@@ -9,9 +11,21 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './game.component.scss',
 })
 export class GameComponent {
+  destoryRef = inject(DestroyRef);
   count: number = 0;
+  autoclickerTotal: number = 0;
 
   increment(): void {
     this.count += 1;
+  }
+
+  startAutoclicker(): void {
+    interval(1000)
+      .pipe(takeUntilDestroyed(this.destoryRef))
+      .subscribe(() => {
+        this.count += 1;
+      });
+
+    this.autoclickerTotal += 1;
   }
 }
