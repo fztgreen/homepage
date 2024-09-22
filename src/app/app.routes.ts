08bf87616c3edfdd89490214c220386fe29e3060
Zachary Greenfield <fztgreen@gmail.com>
2024-09-22 13:20:26 -0500

    feat: Add additional url case matching so any case in url can be used

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,42 @@
-import { Routes } from '@angular/router';
+import { Routes, UrlMatchResult, UrlSegment } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: NavigationComponent },
-  { path: 'game', component: GameComponent },
+  {
+    matcher: (url) => AllCaseMatcher(url, 'details'),
+    redirectTo: '/home#details',
+    pathMatch: 'full',
+  },
+  {
+    matcher: (url) => AllCaseMatcher(url, 'contact'),
+    redirectTo: '/home#contact',
+    pathMatch: 'full',
+  },
+  {
+    matcher: (url) => AllCaseMatcher(url, 'photography'),
+    redirectTo: '/home#photography',
+    pathMatch: 'full',
+  },
+  {
+    matcher: (url) => AllCaseMatcher(url, 'blog'),
+    redirectTo: '/home#blog',
+    pathMatch: 'full',
+  },
+  { matcher: (url) => AllCaseMatcher(url, 'home'), component: NavigationComponent },
+  { matcher: (url) => AllCaseMatcher(url, 'game'), component: GameComponent },
   { path: '**', component: NotFoundComponent },
 ];
+
+function AllCaseMatcher(
+  urlsegments: UrlSegment[],
+  endpoint: string,
+): UrlMatchResult | null {
+  if (urlsegments[0].path.toLocaleLowerCase() == endpoint) {
+    return { consumed: urlsegments };
+  }
+
+  return null;
+}
