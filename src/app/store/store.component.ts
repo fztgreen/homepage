6fcbaf6c8703a694546c7b618befd275eaa9fcbd
Zachary Greenfield <fztgreen@gmail.com>
2024-10-11 16:13:27 -0500

    feat: Add some menu items

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-store',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatMenuModule, MatDividerModule],
   templateUrl: './store.component.html',
-  styleUrl: './store.component.scss'
+  styleUrl: './store.component.scss',
 })
-export class StoreComponent {
-
-}
+export class StoreComponent {}
