423eb196d0037cbd5446ca8a5ad6d27b4952acac
Zachary Greenfield <fztgreen@gmail.com>
2024-10-19 10:07:28 -0500

    refactor: Refactoring projects page to use ngfor

---
+++ src/app/projects/models/project.ts
@@ -0,0 +1,5 @@
+export class Project {
+    public link!: string;
+    public title!: string;
+    public language!: string;
+}
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Project } from './models/project';
 
 @Component({
   selector: 'app-projects',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  readonly projects: Project[] = [
+    {
+      link: "https://github.com/fztgreen/homepage",
+      title: "Homepage",
+      language: "Angular"
+    } satisfies Project,
+    {
+      link: "https://github.com/fztgreen/GroupSeven",
+      title: "The Great Escape Game",
+      language: "c++"
+    } satisfies Project,
+    {
+      link: "https://github.com/fztgreen/ascii",
+      title: "Ascii Art",
+      language: "golang"
+    } satisfies Project,
+    {
+      link: "https://github.com/fztgreen/haskell-example",
+      title: "Haskell Example",
+      language: "haskell"
+    } satisfies Project,
+    {
+      link: "https://github.com/fztgreen/rust-tests-example",
+      title: "Rust Tests Example",
+      language: "rust"
+    } satisfies Project,
+    {
+      link: "https://github.com/fztgreen/KafkaExample",
+      title: "Kafka Example",
+      language: "C#"
+    } satisfies Project,
+    {
+      link: "https://github.com/fztgreen/chat-room",
+      title: "Chat Room",
+      language: "Angular"
+    } satisfies Project,
+    {
+      link: "https://github.com/fztgreen/signal-r",
+      title: "SignalR Example",
+      language: "C#"
+    } satisfies Project,
+    {
+      link: "https://github.com/fztgreen/pong",
+      title: "Pong",
+      language: "c++"
+    } satisfies Project,
+  ];
+}
